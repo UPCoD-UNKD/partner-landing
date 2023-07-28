e8efad6ca09df0b2249a421dfb55223029620a5e
Yurii-Shutkin <yurashutkinn@gmail.com>
2023-07-28 12:45:53 +0300

    create Footer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import Header from './Components/Header/Header';
 import Banner from './Components/Banner/Banner';
 import Parthners from './Common/Parthners/Parthners';
 import MainPage from './Components/MainPage/MainPage';
+import Footer from './Components/Footer/Footer';
+import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 
 const partn = ['OLX', 'PROM', 'ROZETKA', 'ALLO', 'EVO', 'PROZORRO', 'MODNAKASTA', 'ZAKUPKI.COM.UA']
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Banner />
-      <Parthners parthners={partn}/>
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Banner />
+        <Parthners parthners={partn}/>
+        <MainPage />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
