338f092017897c8a6c1b5117ac758802bfbddd68
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-09-25 18:01:27 +0300

    change src/App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import Header from './Components/Header/Header';
-import Banner from './Components/Banner/Banner';
-import Parthners from './Common/Parthners/Parthners';
-import MainPage from './Components/MainPage/MainPage';
-import './App.scss';
+import Header from "./Components/Header/Header";
+import Banner from "./Components/Banner/Banner";
+import Parthners from "./Common/Parthners/Parthners";
+import MainPage from "./Components/MainPage/MainPage";
+import "./App.scss";
 
-const partn = ['OLX', 'PROM', 'ROZETKA', 'ALLO', 'EVO', 'PROZORRO', 'MODNAKASTA', 'ZAKUPKI.COM.UA']
+const partn = [
+  "OLX",
+  "PROM",
+  "ROZETKA",
+  "ALLO",
+  "EVO",
+  "PROZORRO",
+  "MODNAKASTA",
+  "ZAKUPKI.COM.UA",
+];
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
-      <Parthners parthners={partn}/>
+      <Parthners parthners={partn} />
       <MainPage />
     </div>
   );
