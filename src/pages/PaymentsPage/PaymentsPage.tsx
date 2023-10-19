ca81a053369810fbf54b8af75a77640d71adef4e
Oleg Dugaev <dugaevoleg9@gmail.com>
2023-10-19 14:50:33 +0300

    fix

--- src/pages/PaymentsPage/PaymentsPage.tsx
+++ src/pages/PaymentsPage/PaymentsPage.tsx
@@ -1,37 +1,134 @@
-import SideMenu from 'pages/Sidemenu/SideMenu'
-import Navbar from 'shared/ui/navbar/Navbar'
-import { InputFieldSelect, InputFieldSelectStatus, InputsContainer, PaymentsContainer, PaymentsContent } from './PaymentStyles'
-import PaymentsTable from './PaymentsTable'
-import WarningTable from 'shared/ui/warningtable/WarningTable'
+// import SideMenu from 'pages/Sidemenu/SideMenu'
+// import Navbar from 'shared/ui/navbar/Navbar'
+// import { InputFieldSelect, InputFieldSelectStatus, InputsContainer, PaymentsContainer, PaymentsContent } from './PaymentStyles'
+// import PaymentsTable from './PaymentsTable'
+// import WarningTable from 'shared/ui/warningtable/WarningTable'
+// import { useState } from 'react'
 
+
+// const PaymentsPage = () => {
+//     const [selectedStatus, setSelectedStatus] = useState(''); 
+
+//     const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+//         setSelectedStatus(event.target.value);
+//       };
+//       const data = [
+//        'Виплачено',
+//        'Відхилено',
+//        'В процесі'
+//       ];
+//   return (
+//     <div>
+//         <Navbar />
+//         <PaymentsContainer>
+//             <SideMenu />
+//             <PaymentsContent>
+//                 <WarningTable />
+//                 <InputsContainer>
+//                     <InputFieldSelect id="input2">
+//                                 <option value="option1">Останні 30 днів</option>
+//                                 <option value="option2">Останні 40 днів</option>
+//                                 <option value="option3">Останні 50 днів</option>
+//                                 <option value="option4">Останні 60 днів</option>
+//                     </InputFieldSelect>
+//                     <InputFieldSelectStatus id="input2" onChange={handleStatusChange}>
+//                                 <option value="">Статус</option>
+//                                 <option value="Виплачено">Виплачено</option>
+//                                 <option value="Відхилено">Відхилено</option>
+//                                 <option value="В процесі">В процесі</option>
+//                     </InputFieldSelectStatus >
+//                 </InputsContainer>
+//                 <PaymentsTable data={data} selectedStatus={selectedStatus} />
+//             </PaymentsContent>
+//         </PaymentsContainer>
+//     </div>
+//   )
+// }
+
+// export default PaymentsPage
+
+import Navbar from 'shared/ui/navbar/Navbar';
+import SideMenu from 'pages/Sidemenu/SideMenu';
+import WarningTable from 'shared/ui/warningtable/WarningTable';
+import {
+  PaymentsContainer,
+  PaymentsContent,
+} from './PaymentStyles';
+import PaymentsTable from './PaymentsTable';
 
 const PaymentsPage = () => {
+  const paymentData = [
+    {
+      domain: 'mywebsite.net',
+      payoutAmount: 'Сума 1 грн',
+      payoutStatus: 'Виплачено',
+      payoutDate: '11/11/2023',
+      payoutMethod: 'Платіж-чат',
+      reward: '500 ₴',
+    },
+    {
+      domain: 'mywebsite.net',
+      payoutAmount: 'Сума 2 грн',
+      payoutStatus: 'Відхилено',
+      payoutDate: '11/11/2023',
+      payoutMethod: 'Платіж-телефон',
+      reward: '5200 ₴',
+    },
+    {
+      domain: 'testdomain.org',
+      payoutAmount: 'Сума 3 грн',
+      payoutStatus: 'Чекає на підтвердження',
+      payoutDate: '11/11/2023',
+      payoutMethod: 'Нема відомостей',
+      reward: '3500 ₴',
+    },
+    {
+      domain: 'testdomain.org',
+      payoutAmount: 'Сума 3 грн',
+      payoutStatus: 'На розгляді',
+      payoutDate: '11/11/2023',
+      payoutMethod: 'Нема відомостей',
+      reward: '500 ₴',
+    },
+    {
+      domain: 'demo-site.biz',
+      payoutAmount: 'Сума 3 грн',
+      payoutStatus: 'Не активний',
+      payoutDate: '11/11/2023',
+      payoutMethod: 'Нема відомостей',
+      reward: '2500 ₴',
+    },
+    {
+      domain: 'demo-site.biz',
+      payoutAmount: 'Сума 3 грн',
+      payoutStatus: 'Скасовано',
+      payoutDate: '11/11/2023',
+      payoutMethod: 'Нема відомостей',
+      reward: '500 ₴',
+    },
+    {
+      domain: 'partnerwebsite.com',
+      payoutAmount: 'Сума 3 грн',
+      payoutStatus: 'Виплачено',
+      payoutDate: '11/11/2023',
+      payoutMethod: 'Нема відомостей',
+      reward: '1500 ₴',
+    },
+  ];
+
   return (
     <div>
-        <Navbar />
-        <PaymentsContainer>
-            <SideMenu />
-            <PaymentsContent>
-                <WarningTable />
-                <InputsContainer>
-                    <InputFieldSelect id="input2">
-                                <option value="option1">Останні 30 днів</option>
-                                <option value="option2">Останні 40 днів</option>
-                                <option value="option3">Останні 50 днів</option>
-                                <option value="option4">Останні 60 днів</option>
-                    </InputFieldSelect>
-                    <InputFieldSelectStatus id="input2">
-                                <option value="option1">Статус</option>
-                                <option value="option2">Виплачено</option>
-                                <option value="option3">Не виплачено</option>
-                                <option value="option4">В процесі</option>
-                    </InputFieldSelectStatus>
-                </InputsContainer>
-                <PaymentsTable />
-            </PaymentsContent>
-        </PaymentsContainer>
+      <Navbar />
+      <PaymentsContainer>
+        <SideMenu />
+        <PaymentsContent>
+          <WarningTable />
+          
+          <PaymentsTable data={paymentData} />
+        </PaymentsContent>
+      </PaymentsContainer>
     </div>
-  )
-}
+  );
+};
 
-export default PaymentsPage
+export default PaymentsPage;
--- src/pages/PaymentsPage/PaymentsTable.tsx
+++ src/pages/PaymentsPage/PaymentsTable.tsx
@@ -1,205 +1,3 @@
-// import React, { useState } from 'react';
-// import styled from 'styled-components';
-// import moreImg from 'shared/assets/img/PaymentTable/More.svg'
-// import linkImg from 'shared/assets/img/PaymentTable/fi_link.svg'
-// import summaImg from 'shared/assets/img/PaymentTable/fi_align-left.png'
-// import userImg from 'shared/assets/img/PaymentTable/fi_user.svg'
-// import listImg from 'shared/assets/img/PaymentTable/fi_list.png'
-// import loaderImg from 'shared/assets/img/PaymentTable/fi_loader.png'
-// import starImg from 'shared/assets/img/PaymentTable/fi_star.png'
-// import Modal from './Modal';
-
-
-
-// const Table = styled.table`
-//   border-collapse: collapse;
-//   width: 100%;
-//   margin-left: 30px;
-//   margin-top: 15px;
-//   border: 1px #E1E2E2 solid;
-//   box-shadow: 0px 4.080231666564941px 12.240694999694824px rgba(0, 0, 0, 0.15); 
-// `;
-
-// const Th = styled.th`
-//     border: 1px #E1E2E2 solid;
-//     padding: 10px;
-//     text-align: center;
-//     background-color: #FCFCFC;
-//     color: #222222;
-//     font-size: 13.88px;
-//     font-weight: 700;
-//     height: 50px;
-// `;
-
-// const Td = styled.td`
-//     border: 1px #E1E2E2 solid;
-//     padding: 8px;
-//     text-align: center;
-//     color: #222222;
-//     font-size: 12.31px;
-//     font-weight: 400;
-// `;
-
-// const ImageCell = styled.td`
-//   width: 28px;
-//   height: 51px;
-//   text-align: center;
-//   border: 1px #E1E2E2 solid;
-// `;
-
-// const ThImg = styled.img `
-//     width: 15px;
-//     height: 15px;
-//     margin-right: 7px;
-// `
-// const Button= styled.button `
-//     align-items: center;
-// `
-// const data = [
-//   {
-//     domain: 'mywebsite.net',
-//     payoutAmount: 'Сума 1 грн',
-//     payoutStatus: 'Виплачено',
-//     payoutDate: '11/11/2023',
-//     payoutMethod: 'Платіж-чат',
-//     reward: '500 ₴',
-//   },
-//   {
-//     domain: 'mywebsite.net',
-//     payoutAmount: 'Сума 2 грн',
-//     payoutStatus: 'Відхилено',
-//     payoutDate: '11/11/2023',
-//     payoutMethod: 'Платіж-телефон',
-//     reward: '5200 ₴',
-//   },
-//   {
-//     domain: 'testdomain.org',
-//     payoutAmount: 'Сума 3 грн',
-//     payoutStatus: 'Чекає на підтвердження',
-//     payoutDate: '11/11/2023',
-//     payoutMethod: 'Нема відомостей',
-//     reward: '3500 ₴',
-//   },
-//   {
-//     domain: 'testdomain.org',
-//     payoutAmount: 'Сума 3 грн',
-//     payoutStatus: 'На розгляді',
-//     payoutDate: '11/11/2023',
-//     payoutMethod: 'Нема відомостей',
-//     reward: '500 ₴',
-//   },
-//   {
-//     domain: 'demo-site.biz',
-//     payoutAmount: 'Сума 3 грн',
-//     payoutStatus: 'Не активний',
-//     payoutDate: '11/11/2023',
-//     payoutMethod: 'Нема відомостей',
-//     reward: '2500 ₴',
-//   },
-//   {
-//     domain: 'demo-site.biz',
-//     payoutAmount: 'Сума 3 грн',
-//     payoutStatus: 'Скасовано',
-//     payoutDate: '11/11/2023',
-//     payoutMethod: 'Нема відомостей',
-//     reward: '500 ₴',
-//   },
-//   {
-//     domain: 'partnerwebsite.com',
-//     payoutAmount: 'Сума 3 грн',
-//     payoutStatus: 'Виплачено',
-//     payoutDate: '11/11/2023',
-//     payoutMethod: 'Нема відомостей',
-//     reward: '1500 ₴',
-//   },
-
-// ];
-
-// const PaymentsTable: React.FC = () => {
-//     const [isModalOpen, setModalOpen] = useState(false);
-
-//   // Функция для открытия модального окна
-//   const openModal = () => {
-//     setModalOpen(true);
-//   };
-
-//   // Функция для закрытия модального окна
-//   const closeModal = () => {
-//     setModalOpen(false);
-//   };
-
-//   const getStatusStyles = (status:string) => {
-//     // В этой функции определяем стили в зависимости от статуса
-//     switch (status) {
-//       case "Виплачено":
-//         return { background: "#A9CF54" };
-//       case "Відхилено":
-//         return { background: "#FF9385" };
-//       case "Чекає на підтвердження":
-//         return { background: "#A1CDFF" };
-//       case "На розгляді":
-//         return { background: "#FFD78D" };
-//       case "Не активний":
-//         return { background: "#888888" };
-//       case "Скасовано":
-//         return { background: "rgba(195.50, 105.08, 105.08, 0.50)" };
-//       default:
-//         return {}; // Другие статусы могут использовать стандартные стили
-//     }
-//   };
-//   return (
-//     <Table>
-//       <thead>
-//         <tr>
-//           <Th></Th>
-//           <Th>
-//             <ThImg src={linkImg}/>
-//             Домен</Th>
-//           <Th>
-//           <ThImg src={summaImg}/>
-//             Сума виплати</Th>
-//           <Th>
-//           <ThImg src={userImg}/>
-//             Статус виплати</Th>
-//           <Th>
-//           <ThImg src={listImg}/>
-//             Дата виплати</Th>
-//           <Th>
-//           <ThImg src={loaderImg}/>
-//             Спосіб виплат</Th>
-//           <Th>
-//           <ThImg src={starImg}/>
-//             Винагорода</Th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {data.map((item, index) => (
-//           <tr key={index}>
-//             <ImageCell>
-//               <Button onClick={openModal}>
-//                     <img src={moreImg} alt="" />
-//               </Button>
-//             </ImageCell>
-//             <Td>{item.domain}</Td>
-//             <Td>{item.payoutAmount}</Td>
-//             <Td style={getStatusStyles(item.payoutStatus)}>
-//               {item.payoutStatus}
-//             </Td>
-//             <Td>{item.payoutDate}</Td>
-//             <Td>{item.payoutMethod}</Td>
-//             <Td>{item.reward}</Td>
-//           </tr>
-//         ))}
-//       </tbody>
-//       <Modal isOpen={isModalOpen} onClose={closeModal} />
-//     </Table>
-//   );
-// };
-
-// export default PaymentsTable;
-
-
-
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import moreImg from 'shared/assets/img/PaymentTable/More.svg';
@@ -210,6 +8,7 @@ import listImg from 'shared/assets/img/PaymentTable/fi_list.png';
 import loaderImg from 'shared/assets/img/PaymentTable/fi_loader.png';
 import starImg from 'shared/assets/img/PaymentTable/fi_star.png';
 import Modal from './Modal';
+import { InputFieldSelect, InputFieldSelectStatus, InputsContainer } from './PaymentStyles';
 
 const Table = styled.table`
   border-collapse: collapse;
@@ -294,130 +93,100 @@ const Status = ({ status }: { status: string }) => {
   );
 };
 
-const data = [
-  {
-    domain: 'mywebsite.net',
-    payoutAmount: 'Сума 1 грн',
-    payoutStatus: 'Виплачено',
-    payoutDate: '11/11/2023',
-    payoutMethod: 'Платіж-чат',
-    reward: '500 ₴',
-  },
-  {
-    domain: 'mywebsite.net',
-    payoutAmount: 'Сума 2 грн',
-    payoutStatus: 'Відхилено',
-    payoutDate: '11/11/2023',
-    payoutMethod: 'Платіж-телефон',
-    reward: '5200 ₴',
-  },
-  {
-    domain: 'testdomain.org',
-    payoutAmount: 'Сума 3 грн',
-    payoutStatus: 'Чекає на підтвердження',
-    payoutDate: '11/11/2023',
-    payoutMethod: 'Нема відомостей',
-    reward: '3500 ₴',
-  },
-  {
-    domain: 'testdomain.org',
-    payoutAmount: 'Сума 3 грн',
-    payoutStatus: 'На розгляді',
-    payoutDate: '11/11/2023',
-    payoutMethod: 'Нема відомостей',
-    reward: '500 ₴',
-  },
-  {
-    domain: 'demo-site.biz',
-    payoutAmount: 'Сума 3 грн',
-    payoutStatus: 'Не активний',
-    payoutDate: '11/11/2023',
-    payoutMethod: 'Нема відомостей',
-    reward: '2500 ₴',
-  },
-  {
-    domain: 'demo-site.biz',
-    payoutAmount: 'Сума 3 грн',
-    payoutStatus: 'Скасовано',
-    payoutDate: '11/11/2023',
-    payoutMethod: 'Нема відомостей',
-    reward: '500 ₴',
-  },
-  {
-    domain: 'partnerwebsite.com',
-    payoutAmount: 'Сума 3 грн',
-    payoutStatus: 'Виплачено',
-    payoutDate: '11/11/2023',
-    payoutMethod: 'Нема відомостей',
-    reward: '1500 ₴',
-  },
-];
+export type PaymentData = {
+  domain: string;
+  payoutAmount: string;
+  payoutStatus: string;
+  payoutDate: string;
+  payoutMethod: string;
+  reward: string;
+};
 
-const PaymentsTable: React.FC = () => {
+const PaymentsTable: React.FC<{ data: PaymentData[] }> = ({ data }) => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState('');
 
-  // Функция для открытия модального окна
   const openModal = () => {
     setModalOpen(true);
   };
 
-  // Функция для закрытия модального окна
   const closeModal = () => {
     setModalOpen(false);
   };
 
+  const filteredData = selectedStatus
+    ? data.filter((item) => item.payoutStatus === selectedStatus)
+    : data;
+
   return (
-    <Table>
-      <thead>
-        <tr>
-          <Th></Th>
-          <Th>
-            <ThImg src={linkImg} />
-            Домен
-          </Th>
-          <Th>
-            <ThImg src={summaImg} />
-            Сума виплати
-          </Th>
-          <Th>
-            <ThImg src={userImg} />
-            Статус виплати
-          </Th>
-          <Th>
-            <ThImg src={listImg} />
-            Дата виплати
-          </Th>
-          <Th>
-            <ThImg src={loaderImg} />
-            Спосіб виплат
-          </Th>
-          <Th>
-            <ThImg src={starImg} />
-            Винагорода
-          </Th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <ImageCell>
-              <Button onClick={openModal}>
-                <img src={moreImg} alt="" />
-              </Button>
-            </ImageCell>
-            <Td>{item.domain}</Td>
-            <Td>{item.payoutAmount}</Td>
-            <Td>
-              <Status status={item.payoutStatus} />
-            </Td>
-            <Td>{item.payoutDate}</Td>
-            <Td>{item.payoutMethod}</Td>
-            <Td>{item.reward}</Td>
+    <div>
+      <InputsContainer>
+            <InputFieldSelect id="input1">
+              <option value="option1">Останні 30 днів</option>
+              <option value="option2">Останні 40 днів</option>
+              <option value="option3">Останні 50 днів</option>
+              <option value="option4">Останні 60 днів</option>
+            </InputFieldSelect>
+            <InputFieldSelectStatus id="input2" onChange={(e) => setSelectedStatus(e.target.value)}>
+                <option value="">Статус</option>
+                <option value="Виплачено">Виплачено</option>
+                <option value="Відхилено">Відхилено</option>
+                <option value="В процесі">В процесі</option>
+            </InputFieldSelectStatus>
+          </InputsContainer>
+      
+      <Table>
+        <thead>
+          <tr>
+            <Th></Th>
+            <Th>
+              <ThImg src={linkImg} />
+              Домен
+            </Th>
+            <Th>
+              <ThImg src={summaImg} />
+              Сума виплати
+            </Th>
+            <Th>
+              <ThImg src={userImg} />
+              Статус виплати
+            </Th>
+            <Th>
+              <ThImg src={listImg} />
+              Дата виплати
+            </Th>
+            <Th>
+              <ThImg src={loaderImg} />
+              Спосіб виплат
+            </Th>
+            <Th>
+              <ThImg src={starImg} />
+              Винагорода
+            </Th>
           </tr>
-        ))}
-      </tbody>
+        </thead>
+        <tbody>
+          {filteredData.map((item, index) => (
+            <tr key={index}>
+              <ImageCell>
+                <Button onClick={openModal}>
+                  <img src={moreImg} alt="" />
+                </Button>
+              </ImageCell>
+              <Td>{item.domain}</Td>
+              <Td>{item.payoutAmount}</Td>
+              <Td>
+                <Status status={item.payoutStatus} />
+              </Td>
+              <Td>{item.payoutDate}</Td>
+              <Td>{item.payoutMethod}</Td>
+              <Td>{item.reward}</Td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
       <Modal isOpen={isModalOpen} onClose={closeModal} />
-    </Table>
+    </div>
   );
 };
 
